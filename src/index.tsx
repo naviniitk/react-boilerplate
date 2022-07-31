2252154aaad889aad1ff7823e8466843190f7dec
Naveen Kumar <naveen.umar@jalantechnologies.com>
2022-07-31 11:23:28 +0530

    updated react render method acc. to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./app/app";
 
-render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App />);
